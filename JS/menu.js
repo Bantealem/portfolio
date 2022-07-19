e9d8fe5dd00502dddd6b0dd9d1005e30dfe03ee2
Bantealem <bantem2021@gmail.com>
2022-07-19 11:34:21 +0300

    add javascript to html

---
+++ JS/menu.js
@@ -0,0 +1,18 @@
+const hamburgerMenu = document.querySelector('.menu-icon');
+const mobileNavItems = document.querySelectorAll('.mobile-nav-item');
+const mobileClose = document.querySelector('.mobile-menu-close');
+hamburgerMenu.addEventListener('click', () => {
+  // show the mobile menu
+  document.querySelector('.mobile-menu').style.display = 'flex';
+});
+mobileClose.addEventListener('click', () => {
+    console.log("hello")
+  // show the mobile menu
+  document.querySelector('.mobile-menu').style.display = 'none';
+});
+// close menu on click mobile-nav-item
+mobileNavItems.forEach((item) => {
+  item.addEventListener('click', () => {
+    document.querySelector('.mobile-menu').style.display = 'none';
+  });
+});
