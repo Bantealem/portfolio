38ae5772141fdbccf61af4af222a1477dc28bfca
Bantealem <bantem2021@gmail.com>
2022-07-22 23:30:27 +0300

    add form validation

---
+++ JS/form-validation.js
@@ -0,0 +1,8 @@
+document.getElementById('contact').addEventListener('submit', (event) => {
+  const email = document.getElementById('mail').value;
+  if (email !== email.toLowerCase()) {
+    event.preventDefault();
+    document.querySelector('.error').innerHTML = 'Your email address should be in lowercase';
+    event.target.reset();
+  }
+});
